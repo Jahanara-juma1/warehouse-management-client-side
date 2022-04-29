a8182afc6db9aca2aa59763403fc4a4694db5dca
Jahanara-juma1 <jahanarajuma213@gmail.com>
2022-04-29 23:45:49 +0600

    header addad

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <header>
-            <h2>This is header part</h2>
-        </header>
+        <>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand as={Link} to="/">Home</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+
+                            <Nav.Link href="/blogs">Blogs</Nav.Link>
+                            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                        <Nav>
+                        <Nav.Link as={Link} to="about">About</Nav.Link>
+      {/* {
+        user ?
+        <button className='btn btn-link text-white text-decoration-none' onClick={handleSignOut}>Sign out</button>
+        :
+        <Nav.Link as={Link} to="login">
+        Login
+      </Nav.Link>
+      } */}
+    </Nav>
+  </Navbar.Collapse>
+  </Container>
+</Navbar>  
+        </>
     );
 };
 
