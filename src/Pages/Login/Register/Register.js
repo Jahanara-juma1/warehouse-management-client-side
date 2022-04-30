d2a595adc89648a46a305dd2e98c5b771ffc3c93
Jahanara-juma1 <jahanarajuma213@gmail.com>
2022-04-30 09:51:39 +0600

    login part addad

---
+++ src/Pages/Login/Register/Register.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        // <div className='register-form'>
+        //     <h2 style={{textAlign: 'center'}}>Please Register</h2>
+        //     <form onSubmit={handleRegister}>
+        //     <input type="text" name="name" id="" placeholder='Your Name'/>
+                
+        //         <input type="email" name="email" id="" placeholder='Email Address' required/>
+                
+        //         <input type="password" name="password" id="" placeholder='Password' required/>
+        //         <input onClick={() =>setAgree(!agree)} type="checkbox" name="terms" id="terms" />
+        //         <label className={`ps-2 ${agree ? '': 'ps-2 text-danger'}`} htmlFor="terms">Accept Terms and Conditions</label>
+        //         <input
+        //         disabled={!agree}
+        //         className='w-50 mx-auto btn btn-primary mt-2' type="submit" value="Register" />
+        //     </form>
+        //      <p>Already have an account? <Link to="/login" className='text-primary pe-auto' onClick={navigateLogin}>Please Login</Link></p>
+        //      <SocialLogin></SocialLogin>
+        // </div>
+    );
+};
+
+export default Register;
