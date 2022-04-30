d26a3084a00c9fc6115723967d719e9feb888a87
Jahanara-juma1 <jahanarajuma213@gmail.com>
2022-05-01 00:21:51 +0600

    question part addad

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -3,7 +3,27 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div>
-            <h2>this is a page</h2>
+            <h2 className='bg'>Qusetion: Difference between javascript and nodejs</h2>
+            <p>Ans: Javascript- JavaScript is a programming language. It is running in any web browser with a proper browser engine.JavaScript running any engine like Spider monkey (FireFox), JavaScript Core (Safari), V8 (Google Chrome).<br />
+            Nodejs- It is an interpreter and environment for JavaScript with some specific useful libraries which JavaScript programming can use separately.Node JS only run in a V8 engine which mainly used by google chrome. And javascript program which will be written under this Node JS will be always run in V8 Engine.
+            </p>
+            <br />
+            <h2>Qusetion: Differences between sql and nosql databases.</h2>
+            <p>Ans: sql- These databases have fixed or static or predefined schema.These databases are not suited for hierarchical data storage.These databases are best suited for complex queries.Vertically Scalable.Follows ACID property.<br />
+         NoSql- They have dynamic schema.These databases are best suited for hierarchical data storage.These databases are not so good for complex queries.Horizontally scalable.Follows CAP(consistency, availability, partition tolerance).
+
+        </p>
+        <br />
+        <h2>Question: What is the purpose of jwt and how does it work</h2>
+        <p>Ans: JWTs differ from other web tokens in that they contain a set of claims. Claims are used to transmit information between two parties. What these claims are depends on the use case at hand. For example, a claim may assert who issued the token, how long it is valid for, or what permissions the client has been granted.
+
+A JWT is a string made up of three parts, separated by dots (.), and serialized using base64. In the most common serialization format, compact serialization, the JWT looks something like this: xxxxx.yyyyy.zzzzz.
+
+Once decoded, you will get two JSON strings:
+
+The header and the payload.
+The signature. </p>
+
         </div>
     );
 };
--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -99,6 +99,9 @@ import { Link, Navigate, useLocation, useNavigate } from 'react-router-dom';
 import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
 import SocialLogin from '../SocialLogin/SocialLogin';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Loading from '../../../Shared/Loading/Loading';
 
 const Login = () => {
     const emailRef = useRef('');
@@ -118,6 +121,10 @@ const Login = () => {
       const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(
         auth
       );
+
+      if(loading || sending){
+        return <Loading></Loading>
+    }
 
       if(user){
         navigate(from, { replace: true });
@@ -144,10 +151,10 @@ const Login = () => {
     const email = emailRef.current.value;
     if(email){
         await sendPasswordResetEmail(email);
-    // toast('Sent email');
-    // }
-    // else{
-    //     toast('please enter your email address');
+    toast('Sent email');
+    }
+    else{
+        toast('please enter your email address');
     }
 }
 
@@ -176,6 +183,7 @@ const Login = () => {
             <p>Forget Password? <button className='btn btn-link text-primary pe-auto text-decoration-none' onClick={resetPassword}>Reset Password</button></p>
 
             <SocialLogin></SocialLogin>
+            <ToastContainer></ToastContainer>
             
         </div>
         
